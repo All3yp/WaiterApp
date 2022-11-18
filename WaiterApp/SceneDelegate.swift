b33ccd56ecb62cb49af95628de8e732005731f10
All3yp <noyallepereira@gmail.com>
2022-11-18 09:29:06 -0300

    WIP: Navigation

--- WaiterApp/SceneDelegate.swift
+++ WaiterApp/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 		var window: UIWindow?
-	let rootViewController = WaiterMainViewController()
+	let rootViewController = WaiterNavigationControllerFactory.make()
 
 		func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
 							 options connectionOptions: UIScene.ConnectionOptions) {
---
+++ WaiterApp/WaiterNavigationController.swift
@@ -0,0 +1,55 @@
+//
+//  WaiterNavigationController.swift
+//  WaiterApp
+//
+//  Created by Alley Pereira on 18/11/22.
+//
+
+import UIKit
+
+final class WaiterNavigationController: UINavigationController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		self.navigationBar.tintColor = .label
+		self.navigationBar.prefersLargeTitles = true
+		self.navigationBar.backgroundColor = .purple
+
+		setupNavigationAppearance()
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		setRightBarButtonAppearance()
+	}
+
+	private func setupNavigationAppearance() {
+		self.navigationBar.prefersLargeTitles = true
+		self.navigationBar.barTintColor = .purple
+		self.title = "Pedido"
+	}
+
+	private func setRightBarButtonAppearance() {
+		let button = UIBarButtonItem(title: "cancelar pedido", style: .plain, target: self, action: #selector(closeButtonTapped))
+		button.tintColor = UIColor.CustomColor.red
+		self.navigationItem.rightBarButtonItem = button
+	}
+
+	@objc func closeButtonTapped() {
+		self.dismiss(animated: true, completion: nil)
+		print("close button tapped")
+		
+	}
+
+}
+
+final class WaiterNavigationControllerFactory {
+
+	static func make() -> WaiterNavigationController {
+		let navigationController = WaiterNavigationController()
+		let viewController = WaiterMainViewController()
+		navigationController.viewControllers = [viewController]
+		return navigationController
+	}
+}
