8f9784392ba7e1f0f2ac256b870dd0eece5e9b9d
All3yp <noyallepereira@gmail.com>
2022-11-20 23:14:40 -0300

    WIP: Folders Structure

--- WaiterApp/Views/WaiterView.swift
+++ WaiterApp/Views/WaiterView.swift
@@ -1,5 +1,5 @@
 //
-//  WaiterTableView.swift
+//  WaiterView.swift
 //  WaiterApp
 //
 //  Created by Alley Pereira on 18/11/22.
@@ -7,4 +7,4 @@
 
 import UIKit
 
-class WaiterTableView: UITableView {}
+class WaiterView: UIView {}
